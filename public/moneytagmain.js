4405cfc9e0e8216637a46e3b06fd8c948c1295dc
Sayef Ahmed <sayefahmed251@gmail.com>
2026-05-15 23:55:16 +0600

    your message here

--- public/moneytagmain.js
+++ public/moneytagmain.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11010858
+    "zoneId": 11010749
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11010749
+    "zoneId": 11010858
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
